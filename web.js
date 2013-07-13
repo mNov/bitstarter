2d0afd389dda527374a5542c410aed4332261479
Michal Novemsky <novembersky@gmail.com>
2013-07-13 18:58:01 +0000

    now reading from index.html

--- web.js
+++ web.js
@@ -3,7 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFile('index.html', function (err, data) {
+    if(err) throw err;
+    var buffer = new Buffer(256);
+    buffer.write(data, "utf-8");   
+      response.send(buffer.toString);
+ // (response.send('Hello World 2!');
+    }); 
 });
 
 var port = process.env.PORT || 5000;
